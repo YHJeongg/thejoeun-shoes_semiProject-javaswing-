13db85f934434d8580840c3a6e2c7ba061d34b9b
sangwonKim7 <swkim7275@gmail.com>
2022-10-28 12:30:40 +0900

    Manage) Add files about TakeCommand

---
+++ ShoesSemiProject/src/main/java/com/jsplec/hosix/command/HMTakeSearchCommand.java
@@ -0,0 +1,14 @@
+package com.jsplec.hosix.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HMTakeSearchCommand implements HCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ShoesSemiProject/src/main/java/com/jsplec/hosix/dto/TakeDto.java
@@ -0,0 +1,12 @@
+package com.jsplec.hosix.dto;
+
+import java.sql.Timestamp;
+
+public class TakeDto {
+	int tOrderid;
+	String manager_mId;
+	int manufacturer_mfId;
+	int tPrice;
+	int tQty;
+	Timestamp tDate;
+}
