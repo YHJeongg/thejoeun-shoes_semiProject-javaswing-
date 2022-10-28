2c290b564edf180e204349a10e71c8492499a6e9
subin7709 <tnqls103605@naver.com>
2022-10-28 18:48:29 +0900

    add signupInsertCommand, Customer(Dao, Dto), login.jsp, signup.jsp

--- ShoesSemiProject/src/main/java/com/jsplec/hosix/homecontroller/FrontController.java
+++ ShoesSemiProject/src/main/java/com/jsplec/hosix/homecontroller/FrontController.java
@@ -1,16 +1,22 @@
 package com.jsplec.hosix.homecontroller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import com.jsplec.hosix.command.HCommand;
+import com.jsplec.hosix.command.HSignupInsertCommand;
+
 /**
  * Servlet implementation class FrontController
  */
-@WebServlet("/FrontController")
+@WebServlet("*.do")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -18,6 +24,7 @@ public class FrontController extends HttpServlet {
      * Default constructor. 
      */
     public FrontController() {
+    	super();
         // TODO Auto-generated constructor stub
     }
 
@@ -26,15 +33,41 @@ public class FrontController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		System.out.println("doGet");
+		actionDo(request, response);}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("doPost");
+		actionDo(request, response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("actionDo");
+		request.setCharacterEncoding("utf-8");
+
+		String viewPage = null;
+		HCommand command = null;
+
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		System.out.println(com);
+		
+		switch(com) {
+		case("/signup.do"):
+			System.out.println("signup.do");
+			command = new HSignupInsertCommand();
+		    command.execute(request, response);
+		    viewPage="signup.jsp";
+		break;
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
 	}
 
 }
---
+++ ShoesSemiProject/src/main/webapp/signup.jsp
@@ -0,0 +1,67 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="java.sql.*"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Signup AHNSUBIN</title>
+</head>
+
+<body>
+<div style="text-align: center;">
+<br><br>
+	&nbsp;&nbsp;&nbsp;<h2>회원가입</h2>&nbsp;&nbsp;&nbsp;
+</div>
+<form action="signup.do" method="post" >
+	<table style="margin-left: auto; margin-right: auto;">
+	   <tr>
+	      <td>아이디</td>
+	   </tr>
+	   <tr>
+	      <td><input type="text" name="cId" size="40"></td>
+	   </tr>
+	   <tr>
+	      <td>비밀번호</td>
+	   </tr>
+	   <tr>
+	      <td><input type="text" name="cPw" size="40"></td>
+	   </tr>
+	   <tr>
+	      <td>비밀번호 재확인</td>
+	   </tr>
+	   <tr>
+	      <td><input type="text" name="cPw1" size="40"></td>
+	   </tr>
+	   <tr>
+	      <td>이름</td>
+	   </tr>
+	   <tr>
+	      <td><input type="text" name="cName" size="40"></td>
+	   </tr>
+	   <tr>
+	      <td>휴대전화</td>
+	   </tr>
+	   <tr>
+	      <td><input type="text" name="cTelno" size="40"></td>
+	   </tr>
+	   <tr>
+	      <td>이메일</td>
+	   </tr>
+	   <tr>
+	      <td><input type="text" name="cTelno" size="40"></td>
+	   </tr>
+	   <tr>
+	      <td>주소</td>
+	   </tr>
+	   <tr>
+	      <td><input type="text" name="cAddress" size="40"></td>
+	   </tr>
+	   <tr>
+	      <td><input type="submit" value="회원가입하기" style="WIDTH: 210pt; HEIGHT: 20pt"><a href="Main.do">홈으로 돌아가기</a></td>
+	   </tr>
+	</table>
+	</form>
+</body>
+</html>
