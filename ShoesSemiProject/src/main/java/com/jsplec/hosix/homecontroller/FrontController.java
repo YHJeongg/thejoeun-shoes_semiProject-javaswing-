50d02c8065c1d995d768b163ef2c2b83252c79f6
YunHyeon Jeong <97114061+YHJeongg@users.noreply.github.com>
2022-10-30 22:28:40 +0900

    Update FrontController.java

--- ShoesSemiProject/src/main/java/com/jsplec/hosix/homecontroller/FrontController.java
+++ ShoesSemiProject/src/main/java/com/jsplec/hosix/homecontroller/FrontController.java
@@ -63,72 +63,7 @@ public class FrontController extends HttpServlet {
 		String uri=request.getRequestURI();//8080포트번호 뒤에 있는 주소를 가져오는 역할을 함
 		String conPath=request.getContextPath();//프로젝트 폴더명을 보여줌
 		String com=uri.substring(conPath.length());//입력한 값만 받아서 보여줌
-		System.out.println(com);
-		
-		switch(com) {
-		case("/manage_product.do")://list.do라고 작성을하면 작업을 이어서 하라
-			
-			command=new HMPListCommand();
-			command.execute(request, response);//BListCommand가 구동되도록 해줌
-			viewPage="manage_product.jsp";//여기로 이동하겠다.
-			break;
-		
-		case("/product_select.do")://manage_product에 띄워져있던 정보를 가져와서 보여주겠다.
-			System.out.println("product_select에 들어옵니다.");
-			command =new HMPSeenCommand();
-			command.execute(request, response);//넣음
-			viewPage="manage_product_modify.jsp";//여기로 이동하겠다.
-			break;
-		case("/manage_product_modify.do")://수정하기	
-			command =new HMPModifyCommand();
-			command.execute(request, response);//넣음
-			viewPage="manage_product.do";//수정되고 나서 처음 화면으로 돌아가서 수정된 내역 보여주기
-		
-			break;//입력화면 띄우기
-		case("/HMPproduct_delete.do")://삭제하기
-			System.out.println("HMPproduct_delete에 들어왔습니다.");
-			command =new HMPDeleteCommand();
-			command.execute(request, response);//넣음
-			viewPage="manage_product.do";//보여주기
-		
-			break;//입력화면 띄우기
-		case("/manage_product_search.do"):
-			System.out.println("manage_product_search에 들어왔습니다.");
-			command =new HMPSearchCommand();
-			command.execute(request, response);//넣음
-			viewPage="manage_product.jsp";//보여주기
-		
-			break;//입력화면 띄우기
-			
-		case("/HMPproduct_insert.do")://입력하기
-			//****코딩을 시작하겠습니다.
-			
-			command =new HMPInsertCommand();
-			System.out.println("HMPproduct_insert에들어왔습니다.");
-			command.execute(request, response);//넣음
-			viewPage="manage_product.do";//보여주기
-		
-			break;//입력화면 띄우기insert는 생각해보기
-			
-		case("/manage_customer_order.do")://주문 정보
-			System.out.println("manage_customer_order에 들어왔습니다.");
-			command =new CustomerOrderCommand();
-			command.execute(request, response);//넣음
-			viewPage="manage_customer_order.jsp";//보여주기
-			
-			break;
-			
-		case("/manage_customer_info.do")://주문자 정보
-			System.out.println("manage_customer_info에 들어왔습니다.");
-			command =new CustomerOrderInfoCommand();
-			command.execute(request, response);//넣음
-			viewPage="manage_customer_info.jsp";//보여주기
-			
-			break;
-		}
-		
-		
-		
+	
 		RequestDispatcher dispatcher=request.getRequestDispatcher(viewPage);
 		dispatcher.forward(request, response);
 	}
