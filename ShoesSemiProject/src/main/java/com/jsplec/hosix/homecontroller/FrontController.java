6b29b46aa0264512c17a6c5a395c59707f5844f7
sangwonKim7 <swkim7275@gmail.com>
2022-10-31 10:41:16 +0900

    Merge songMC into sangwonKim7

--- ShoesSemiProject/src/main/java/com/jsplec/hosix/homecontroller/FrontController.java
+++ ShoesSemiProject/src/main/java/com/jsplec/hosix/homecontroller/FrontController.java
@@ -1,118 +1,187 @@
-//package com.jsplec.hosix.homecontroller;
-//
-//import java.io.IOException;
-//
-//import javax.servlet.RequestDispatcher;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import com.jsplec.hosix.command.HCommand;
-//import com.jsplec.hosix.command.HMCustomerDeleteCommand;
-//import com.jsplec.hosix.command.HMCustomerListCommand;
-//import com.jsplec.hosix.command.HMCustomerSearchCommand;
-//import com.jsplec.hosix.command.HMTakeActionCommand;
-//import com.jsplec.hosix.command.HMTakeListCommand;
-//import com.jsplec.hosix.command.HMTakeSearchCommand;
-//
-///**
-// * Servlet implementation class FrontController
-// */
-//@WebServlet("*.do")
-//public class FrontController extends HttpServlet {
-//	private static final long serialVersionUID = 1L;
-//       
-//    /**
-//     * @see HttpServlet#HttpServlet()
-//     */
-//    public FrontController() {
-//        super();
-//        // TODO Auto-generated constructor stub
-//    }
-//
-//	/**
-//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		System.out.println("doGet");
-//		actionDo(request, response);
-//	}
-//
-//	/**
-//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		System.out.println("doPost");
-//		actionDo(request, response);
-//	}
-//	
-//	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		System.out.println("actionDo");
-//		request.setCharacterEncoding("utf-8");
-//		
-//		String viewPage = null;
-//		HCommand command = null;
-//		
-//		String uri = request.getRequestURI();
-//		String conPath = request.getContextPath();
-//		String com = uri.substring(conPath.length());
-//		System.out.println(com);
-//		
-//		switch (com) {
-//		// 발주 전체 리스트 검색
-//		case ("/manage_take_list.do"):
-//			command = new HMTakeListCommand();
-//			command.execute(request, response);
-//			viewPage = "manage_take.jsp";
-//			break;
-//
-//		// 발주 선택 검색
-//		case ("/manage_take_search.do"):
-//			command = new HMTakeSearchCommand();
-//			command.execute(request, response);
-//			viewPage = "manage_take.jsp";
-//			break;
-//
-//		// 발주
-//		case ("/manage_take_action.do"):
-//			command = new HMTakeActionCommand();
-//			command.execute(request, response);
-//			viewPage = "manage_take_list.do";
-//			break;
-//
-//		// 발주 회원 리스트 검색
-//		case ("/manage_customer_list.do"):
-//			System.out.println("custListController");
-//			command = new HMCustomerListCommand();
-//			command.execute(request, response);
-//			viewPage = "manage_customer_list.jsp";
-//			break;
-//
-//		// 회원 선택검색
-//		case ("/manage_customer_search.do"):
-//			command = new HMCustomerSearchCommand();
-//			command.execute(request, response);
-//			viewPage = "manage_customer_list.jsp";
-//			break;
-//
-//		// 회원삭제
-//		case ("/manage_customer_delete.do"):
-//			command = new HMCustomerDeleteCommand();
-//			command.execute(request, response);
-//			viewPage = "manage_customer_list.do";
-//			break;
-//
-//		} //switch
-//		
-//		
-//		
-//		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
-//		dispatcher.forward(request, response);
-//	}
-//	
-//
-//}
+package com.jsplec.hosix.homecontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jsplec.hosix.command.CustomerOrderCommand;
+import com.jsplec.hosix.command.CustomerOrderInfoCommand;
+import com.jsplec.hosix.command.HCommand;
+import com.jsplec.hosix.command.HMCustomerDeleteCommand;
+import com.jsplec.hosix.command.HMCustomerListCommand;
+import com.jsplec.hosix.command.HMCustomerSearchCommand;
+import com.jsplec.hosix.command.HMPDeleteCommand;
+import com.jsplec.hosix.command.HMPInsertCommand;
+import com.jsplec.hosix.command.HMPListCommand;
+import com.jsplec.hosix.command.HMPModifyCommand;
+import com.jsplec.hosix.command.HMPSearchCommand;
+import com.jsplec.hosix.command.HMPSeenCommand;
+import com.jsplec.hosix.command.HMTakeActionCommand;
+import com.jsplec.hosix.command.HMTakeListCommand;
+import com.jsplec.hosix.command.HMTakeSearchCommand;
+
+/**
+ * Servlet implementation class FrontController
+ */
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FrontController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+		actionDo(request, response);
+	}
+
+	private void actionDo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("actionDo");
+		request.setCharacterEncoding("utf-8");
+
+		String viewPage = null;
+		HCommand command = null;
+
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		System.out.println(com);
+
+		switch (com) {
+		// ---------------- 상 콘트롤러 Start -------------------------------------------------
+		// 발주 전체 리스트 검색
+		case ("/manage_take_list.do"):
+			command = new HMTakeListCommand();
+			command.execute(request, response);
+			viewPage = "manage_take.jsp";
+			break;
+
+		// 발주 선택 검색
+		case ("/manage_take_search.do"):
+			command = new HMTakeSearchCommand();
+			command.execute(request, response);
+			viewPage = "manage_take.jsp";
+			break;
+
+		// 발주
+		case ("/manage_take_action.do"):
+			command = new HMTakeActionCommand();
+			command.execute(request, response);
+			viewPage = "manage_take_list.do";
+			break;
+
+		// 발주 회원 리스트 검색
+		case ("/manage_customer_list.do"):
+			System.out.println("custListController");
+			command = new HMCustomerListCommand();
+			command.execute(request, response);
+			viewPage = "manage_customer_list.jsp";
+			break;
+
+		// 회원 선택검색
+		case ("/manage_customer_search.do"):
+			command = new HMCustomerSearchCommand();
+			command.execute(request, response);
+			viewPage = "manage_customer_list.jsp";
+			break;
+
+		// 회원삭제
+		case ("/manage_customer_delete.do"):
+			command = new HMCustomerDeleteCommand();
+			command.execute(request, response);
+			viewPage = "manage_customer_list.do";
+			break;
+		// ---------------- 상원 콘트롤러 End -------------------------------------------------
+		// ---------------- 명철 콘트롤러 Start -------------------------------------------------
+		case ("/manage_product.do"):// list.do라고 작성을하면 작업을 이어서 하라
+			command = new HMPListCommand();
+			command.execute(request, response);// BListCommand가 구동되도록 해줌
+			viewPage = "manage_product.jsp";// 여기로 이동하겠다.
+			break;
+
+		case ("/product_select.do"):// manage_product에 띄워져있던 정보를 가져와서 보여주겠다.
+			System.out.println("product_select에 들어옵니다.");
+			command = new HMPSeenCommand();
+			command.execute(request, response);// 넣음
+			viewPage = "manage_product_modify.jsp";// 여기로 이동하겠다.
+			break;
+			
+		case ("/manage_product_modify.do"):// 수정하기
+			command = new HMPModifyCommand();
+			command.execute(request, response);// 넣음
+			viewPage = "manage_product.do";// 수정되고 나서 처음 화면으로 돌아가서 수정된 내역 보여주기
+			break;// 입력화면 띄우기
+			
+		case ("/HMPproduct_delete.do"):// 삭제하기
+			System.out.println("HMPproduct_delete에 들어왔습니다.");
+			command = new HMPDeleteCommand();
+			command.execute(request, response);// 넣음
+			viewPage = "manage_product.do";// 보여주기
+			break;// 입력화면 띄우기
+			
+		case ("/manage_product_search.do"):
+			System.out.println("manage_product_search에 들어왔습니다.");
+			command = new HMPSearchCommand();
+			command.execute(request, response);// 넣음
+			viewPage = "manage_product.jsp";// 보여주기
+			break;// 입력화면 띄우기
+
+		case ("/HMPproduct_insert.do"):// 입력하기
+			// ****코딩을 시작하겠습니다.
+			command = new HMPInsertCommand();
+			System.out.println("HMPproduct_insert에들어왔습니다.");
+			command.execute(request, response);// 넣음
+			viewPage = "manage_product.do";// 보여주기
+			break;// 입력화면 띄우기insert는 생각해보기
+
+		case ("/manage_customer_order.do"):// 주문 정보
+			System.out.println("manage_customer_order에 들어왔습니다.");
+			command = new CustomerOrderCommand();
+			command.execute(request, response);// 넣음
+			viewPage = "manage_customer_order.jsp";// 보여주기
+			break;
+
+		case ("/manage_customer_info.do"):// 주문자 정보
+			System.out.println("manage_customer_info에 들어왔습니다.");
+			command = new CustomerOrderInfoCommand();
+			command.execute(request, response);// 넣음
+			viewPage = "manage_customer_info.jsp";// 보여주기
+			break;
+		// ---------------- 명철 콘트롤러 End Line --------------------------------------
+
+
+		} // switch
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+
+}
